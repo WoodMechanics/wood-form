44375ef31ee12c8659b63c717d4dc568d447dc76
Winechicken <123495320+Winechicken@users.noreply.github.com>
2023-12-15 11:35:53 +0400

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+	<main>
+		Какой-нибудь текст
+	</main>
+</template>
+
+<style>
+	body{
+		display:flex;
+		align-items: center;
+		justify-content: center;
+}
+</style>
