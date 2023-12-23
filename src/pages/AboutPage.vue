50cf2e0af992abdaa0c09fc10f19959099644a5c
Winechicken <dimonvinokurov@gmail.com>
2023-12-23 22:11:01 +0400

    added style for AboutPage

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -13,9 +13,13 @@
 
 <style>
 	body{
+		margin: 5% 0 0 0;
+		background-color: #efeeee;
 		color: #181818;
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		height: 100%;
+		width: 100%;
 }
 </style>
