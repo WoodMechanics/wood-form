92558e72cad9df280064a12b0769adc802b52d24
Winechicken <dimonvinokurov@gmail.com>
2023-12-23 13:53:58 +0400

    Added routing to the About page by Header.vue file creation

---
+++ src/components/about/header/Header.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+	import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+	<nav>
+		<a href="">Overview</a>
+		<a href="">Faatures</a>
+		<RouterLink to="/sign-in">Sign In</RouterLink>
+		<a href=""></a>
+	</nav>
+</template>
+
+<style>
+	nav{
+    background-color: #a1a1a1;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePage from '../pages/HomePage.vue'
+import AboutPage from '../pages/AboutPage.vue'
+import SignInPage from '../pages/SignInPage.vue'
+
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +10,17 @@ const router = createRouter({
 		{
 			path: '/',
 			name: 'home',
-			component: HomeView
+			component: HomePage
+		},
+		{
+			path: '/about',
+			name: 'about',
+			component: AboutPage
+		},
+		{
+			path: '/sign-in',
+			name: 'sign-in',
+			component: SignInPage
 		},
 	]
 })
