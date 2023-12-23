2a5ad93fd286a7629966e433a880e62b2362b9c6
Winechicken <dimonvinokurov@gmail.com>
2023-12-23 22:14:00 +0400

    Structured nav-bar in Header component

--- src/components/about/header/Header.vue
+++ src/components/about/header/Header.vue
@@ -1,19 +1,56 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-	import { RouterLink } from 'vue-router'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-	<nav>
-		<a href="">Overview</a>
-		<a href="">Faatures</a>
-		<RouterLink to="/sign-in">Sign In</RouterLink>
-		<a href=""></a>
-	</nav>
+	<body>
+		<nav>
+			<div>
+				<a href="#">Overview</a>
+				<a href="#">Features</a>
+			</div>
+			<div>
+				<RouterLink to="/sign-in">Sign In</RouterLink>
+				<RouterLink to="/" class="mainLink">Go to Forms</RouterLink>
+			</div>
+		</nav>
+	</body>
 </template>
 
 <style>
-	nav{
-    background-color: #a1a1a1;
+@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap');
+nav {
+	width: 100%;
+	background-color: #efeeee;
+	box-shadow: 0 2px 5px #9d9d9d;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	position: fixed;
+	top: 0;
+	padding: 20px;
+	font-family: 'Red Hat Display', sans-serif;
 }
+a {
+	text-decoration: none;
+	color: #121212;
+	margin: 10px 40px;
+	font-size: 20px;
+}
+a:hover{
+	transition-duration: .4s;
+	color: #28297a;
+	text-shadow: 0 0 1px #28297a;
+}
+.mainLink{
+	background-color: #d0d0d0;
+	border: 1px solid #b3b3b3;
+	padding: 10px 20px;
+	border-radius: 5px;
+}
+.mainLink:hover{
+	border: 1px solid #28297a;
+}
+
 </style>
