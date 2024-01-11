548b0c1e356dad3ff42f5e820ec1c8c549ca4456
Winechicken <dimonvinokurov@gmail.com>
2024-01-11 17:21:07 +0400

    Applied font style

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,28 +1,25 @@
 <script setup>
-	import Header from '../components/about/header/Header.vue'
+import Header from '../components/about/header/Header.vue'
 </script>
 
 <template>
-	<Header>
-</Header>
+	<Header> </Header>
 
-	<main>
-		About
-	</main>
+	<main>About</main>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap');
-
-	body{
-		font-family: 'Red Hat Display', sans-serif;
-		margin: 5% 0 0 0;
-		background-color: #efeeee;
-		color: #181818;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 100%;
-		width: 100%;
+body {
+	margin: 5% 0 0 0;
+	background-color: #efeeee;
+	color: #181818;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+	width: 100%;
+}
+main {
+	font-family: var(--font-bold);
 }
 </style>
