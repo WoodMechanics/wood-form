34bbebf3c15db657347331affa98323eb160dc2f
Winechicken <dimonvinokurov@gmail.com>
2024-01-11 17:24:40 +0400

    Fixed according to previous discussions

--- src/components/about/header/Header.vue
+++ src/components/about/header/Header.vue
@@ -1,6 +1,6 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
 import { RouterLink } from 'vue-router'
+import LinkButton from '../../common/LinkButton.vue'
 </script>
 
 <template>
@@ -10,16 +10,15 @@ import { RouterLink } from 'vue-router'
 				<a href="#">Overview</a>
 				<a href="#">Features</a>
 			</div>
-			<div>
+			<div class="rightSide">
 				<RouterLink to="/sign-in">Sign In</RouterLink>
-				<RouterLink to="/" class="mainLink">Go to Forms</RouterLink>
+				<LinkButton></LinkButton>
 			</div>
 		</nav>
 	</body>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap');
 nav {
 	width: 100%;
 	background-color: #efeeee;
@@ -30,27 +29,23 @@ nav {
 	position: fixed;
 	top: 0;
 	padding: 20px;
-	font-family: 'Red Hat Display', sans-serif;
+	font-family: var(--font-main);
 }
+
 a {
 	text-decoration: none;
 	color: #121212;
 	margin: 10px 40px;
 	font-size: 20px;
-}
-a:hover{
-	transition-duration: .4s;
-	color: #28297a;
-	text-shadow: 0 0 1px #28297a;
-}
-.mainLink{
-	background-color: #d0d0d0;
-	border: 1px solid #b3b3b3;
-	padding: 10px 20px;
-	border-radius: 5px;
-}
-.mainLink:hover{
-	border: 1px solid #28297a;
-}
 
+	&:hover {
+		transition-duration: 0.4s;
+		color: #28297a;
+		text-shadow: 0 0 1px #28297a;
+	}
+}
+.rightSide {
+	display: flex;
+	margin-right: 1%;
+}
 </style>
