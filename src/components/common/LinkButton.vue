9c471671197076f821c0346fac5f316e2a06b532
Winechicken <dimonvinokurov@gmail.com>
2024-01-11 17:24:17 +0400

    Fixed according to previous discussions

--- src/components/common/LinkButton.vue
+++ src/components/common/LinkButton.vue
@@ -1,7 +1,20 @@
 <script setup></script>
 
 <template>
-	<div></div>
+	<div class="mainLink">Go to Forms</div>
 </template>
 
-<style></style>
+<style scoped>
+.mainLink {
+	background-color: #d0d0d0;
+	border: 1px solid #b3b3b3;
+	padding: 10px 20px;
+	border-radius: 5px;
+
+	&:hover {
+		color: #28297a;
+		border: 1px solid #28297a;
+		transition-duration: 0.4s;
+	}
+}
+</style>
