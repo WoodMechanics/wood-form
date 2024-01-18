4eed9d31dd7bf6d84b8ef05eb14aba757f5355f2
Winechicken <dimonvinokurov@gmail.com>
2024-01-18 11:12:33 +0400

    Mistakes correction

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-
-	import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-
 	<RouterView />
 </template>
 
@@ -14,24 +12,11 @@ header {
 	max-height: 100vh;
 }
 
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
 nav {
 	width: 100%;
 	font-size: 12px;
 	text-align: center;
 	margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-	color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-	background-color: transparent;
 }
 
 nav a {
@@ -50,17 +35,6 @@ nav a:first-of-type {
 		place-items: center;
 		padding-right: calc(var(--section-gap) / 2);
 	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
-
 	nav {
 		text-align: left;
 		margin-left: -1rem;
