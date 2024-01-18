d6c0e95feae5c276a46995452567f7936a6b9ea5
Winechicken <dimonvinokurov@gmail.com>
2024-01-18 11:15:23 +0400

    Deleted unnesessary styles

--- src/App.vue
+++ src/App.vue
@@ -6,42 +6,4 @@ import { RouterView } from 'vue-router'
 	<RouterView />
 </template>
 
-<style scoped>
-header {
-	line-height: 1.5;
-	max-height: 100vh;
-}
-
-nav {
-	width: 100%;
-	font-size: 12px;
-	text-align: center;
-	margin-top: 2rem;
-}
-
-nav a {
-	display: inline-block;
-	padding: 0 1rem;
-	border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-	border: 0;
-}
-
-@media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-	nav {
-		text-align: left;
-		margin-left: -1rem;
-		font-size: 1rem;
-
-		padding: 1rem 0;
-		margin-top: 1rem;
-	}
-}
-</style>
+<style scoped></style>
