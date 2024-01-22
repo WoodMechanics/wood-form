f8e700a8064aee9b5418bd68e4d8e1716f0099af
Winechicken <dimonvinokurov@gmail.com>
2024-01-23 00:01:13 +0400

    Adjusted AboutPage component due to new components and styles

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,244 +1,172 @@
 <script setup>
 import Header from '../components/about/header/Header.vue'
 import LinkButton from '../components/common/LinkButton.vue'
+import BackGround from '../components/common/BackGround.vue'
+import WoodMechanics from '../components/common/WoodMechanics.vue'
+import Section from '../components/about/section/Section.vue'
 </script>
 
 <template>
-	<Header> </Header>
-	<section id="overview">
-		<div>
-			<h1>Get insights quickly, with Wood-Forms</h1>
-			<p>Easily create and share online forms and surveys, and analyze responses in real-time.</p>
-			<LinkButton></LinkButton>
-		</div>
-		<img src="" alt="" class="image" />
-		<img src="../assets/tree1.png" alt="" class="backgroundPic" />
-	</section>
-	<section class="section2">
-		<p>
-			<a href="#features" class="downLink"
-				>See what you can do with Wood-Forms <br />
-				<img src="../assets/icons8-arrow-down-30.png" alt=""
-			/></a>
-		</p>
-		<img src="../assets/tree2.png" alt="" class="backgroundPic" />
-	</section>
-	<section id="features">
-		<img src="" alt="" class="image" />
-		<div>
-			<h2>Create an online form as easily as creating a document</h2>
-			<p>
-				Select from multiple question types, drag-and-drop to reorder questions, and customize
-				values as easily as pasting a list.
-			</p>
-		</div>
-		<img src="../assets/wood2.png" alt="" class="backgroundPic" />
-	</section>
-	<section class="section4">
-		<div>
-			<h2>Send polished surveys and forms</h2>
-			<p>
-				Customize colors, images, and fonts to adjust the look and feel or reflect your
-				organization’s branding. And add custom logic that shows questions based on answers, for a
-				more seamless experience.
-			</p>
-		</div>
-		<img src="" alt="" class="image" />
-		<img src="../assets/tree4.png" alt="" class="backgroundPic" />
-	</section>
-	<section>
-		<img src="" alt="" class="image" />
-		<div>
-			<h2>Analyze responses with automatic summaries</h2>
-			<p>
-				See charts with response data update in real-time. Or open the raw data with Google Sheets
-				for deeper analysis or automation.
-			</p>
-		</div>
-	</section>
-	<section class="section5">
-		<div>
-			<h2>Create and respond to surveys from anywhere</h2>
-			<p>
-				Access, create, and edit forms on-the-go, from screens big and small. Others can respond to
-				your survey from wherever they are—from any mobile device, tablet, or computer.
-			</p>
-		</div>
-		<img src="" alt="" class="image phoneImg" />
-		<img src="../assets/wood1.png" alt="" class="backgroundPic" />
-	</section>
-	<section class="section6">
-		<div>
-			<img
-				src="https://kstatic.googleusercontent.com/files/e6cc39df1fc54b2cf1f86e429554f094be916ed5d65856b9ebbf2745577678aae2ec0549802ef14b997b6de066ebe3f2fde97ea98cb0f67c35d500708f368b87"
-				alt=""
-			/>
-			<h4>Build forms and analyze results together</h4>
-			<p>
-				Add collaborators to build questions together in real-time. Then analyze results together
-				without having to share multiple versions of the file.
-			</p>
-		</div>
-		<div>
-			<img
-				src="https://kstatic.googleusercontent.com/files/ff3f08e4e57df98669aea7a40dd55ed3eee93b00b4e661d076a820ee4ca9165f6a810bf1071093c8662f49402fe30ae56cbbaa700b0c08d15ff8aec5315beea1"
-				alt=""
-			/>
-			<h4>Work with clean response data</h4>
-			<p>
-				Use built-in intelligence to set response validation rules. For example, ensure that email
-				addresses are properly formatted or that numbers fall within a specified range.
-			</p>
-		</div>
-		<div>
-			<img
-				src="https://kstatic.googleusercontent.com/files/cff121456527424642798945932a09974a2fa0d0f95e838968bcaeed4513456885478939f9ffb06ebb03dd32e7ddd3dea4bcd2897fc9d956907aab45033bd1d0"
-				alt=""
-			/>
-			<h4>Share forms via email, link, or website</h4>
-			<p>
-				It's easy to share forms with specific people or with a broad audience by embedding forms on
-				your website or sharing the links on social media.
-			</p>
-		</div>
-	</section>
-	<section class="section7">
-		<div class="heading">
-			<h2>Get a head start with templates</h2>
-			<p>
-				Choose from a variety of surveys, questionnaires, and other professionally-designed
-				templates to kick things off quickly.
-			</p>
-		</div>
-		<img src="../assets/tree3.png" alt="" class="backgroundPic" />
-		<div class="section7Images">
+	<BackGround />
+	<Header />
+	<main>
+		<Section id="overview">
 			<div>
-				<p>Orders</p>
-				<img src="" alt="" class="section7Img" />
+				<h1>Get insights quickly, with Wood-Forms</h1>
+				<p>Easily create and share online forms and surveys, and analyze responses in real-time.</p>
+				<LinkButton />
+			</div>
+			<div class="image" />
+		</Section>
+		<Section class="section2">
+			<p>
+				<a href="#features" class="downLink"
+					>See what you can do with Wood-Forms <br />
+					<img src="../assets/icons8-arrow-down-30.png" alt=""
+				/></a>
+			</p>
+		</Section>
+		<Section id="features">
+			<div class="image" />
+			<div>
+				<h2>Create an online form as easily as creating a document</h2>
+				<p>
+					Select from multiple question types, drag-and-drop to reorder questions, and customize
+					values as easily as pasting a list.
+				</p>
+			</div>
+		</Section>
+		<Section>
+			<div>
+				<h2>Send polished surveys and forms</h2>
+				<p>
+					Customize colors, images, and fonts to adjust the look and feel or reflect your
+					organization's branding. And add custom logic that shows questions based on answers, for a
+					more seamless experience.
+				</p>
+			</div>
+			<div class="image" />
+		</Section>
+		<Section>
+			<div class="image" />
+			<div>
+				<h2>Analyze responses with automatic summaries</h2>
+				<p>
+					See charts with response data update in real-time. Or open the raw data with Google Sheets
+					for deeper analysis or automation.
+				</p>
+			</div>
+		</Section>
+		<Section>
+			<div>
+				<h2>Create and respond to surveys from anywhere</h2>
+				<p>
+					Access, create, and edit forms on-the-go, from screens big and small. Others can respond
+					to your survey from wherever they are—from any mobile device, tablet, or computer.
+				</p>
+			</div>
+			<div class="image phoneImg" />
+		</Section>
+		<Section class="section6">
+			<div>
+				<img
+					src="https://kstatic.googleusercontent.com/files/e6cc39df1fc54b2cf1f86e429554f094be916ed5d65856b9ebbf2745577678aae2ec0549802ef14b997b6de066ebe3f2fde97ea98cb0f67c35d500708f368b87"
+					alt=""
+				/>
+				<h4>Build forms and analyze results together</h4>
+				<p>
+					Add collaborators to build questions together in real-time. Then analyze results together
+					without having to share multiple versions of the file.
+				</p>
 			</div>
 			<div>
-				<p>Registration</p>
-				<img src="" alt="" class="section7Img" />
+				<img
+					src="https://kstatic.googleusercontent.com/files/ff3f08e4e57df98669aea7a40dd55ed3eee93b00b4e661d076a820ee4ca9165f6a810bf1071093c8662f49402fe30ae56cbbaa700b0c08d15ff8aec5315beea1"
+					alt=""
+				/>
+				<h4>Work with clean response data</h4>
+				<p>
+					Use built-in intelligence to set response validation rules. For example, ensure that email
+					addresses are properly formatted or that numbers fall within a specified range.
+				</p>
 			</div>
 			<div>
-				<p>Feedback</p>
-				<img src="" alt="" class="section7Img" />
+				<img
+					src="https://kstatic.googleusercontent.com/files/cff121456527424642798945932a09974a2fa0d0f95e838968bcaeed4513456885478939f9ffb06ebb03dd32e7ddd3dea4bcd2897fc9d956907aab45033bd1d0"
+					alt=""
+				/>
+				<h4>Share forms via email, link, or website</h4>
+				<p>
+					It's easy to share forms with specific people or with a broad audience by embedding forms
+					on your website or sharing the links on social media.
+				</p>
 			</div>
-			<div>
-				<p>Assessment</p>
-				<img src="" alt="" class="section7Img" />
+		</Section>
+		<Section class="section7">
+			<div class="sectionHead">
+				<h2>Get a head start with templates</h2>
+				<p>
+					Choose from a variety of surveys, questionnaires, and other professionally-designed
+					templates to kick things off quickly.
+				</p>
 			</div>
-		</div>
-	</section>
-	<section class="section8">
-		<h2>Ready to get started?</h2>
-		<LinkButton></LinkButton>
-		<img src="../assets/wood3.png" alt="" class="backgroundPic" />
-	</section>
+			<div class="section7Images">
+				<div>
+					<p>Orders</p>
+					<div class="section7Img" />
+				</div>
+				<div>
+					<p>Registration</p>
+					<div class="section7Img" />
+				</div>
+				<div>
+					<p>Feedback</p>
+					<div class="section7Img" />
+				</div>
+				<div>
+					<p>Assessment</p>
+					<div class="section7Img" />
+				</div>
+			</div>
+		</Section>
+		<Section class="section8">
+			<h2>Ready to get started?</h2>
+			<LinkButton />
+		</Section>
+		<WoodMechanics />
+	</main>
 </template>
 
 <style>
-html {
+main {
 	width: 100%;
-	scroll-behavior: smooth;
-}
-body {
-	margin: 2% 0 0 0;
-	background-color: var(--color-background);
-	color: var(--color-text);
-	display: flex;
-	align-items: center;
-	justify-content: center;
 	height: 100%;
-	width: 100%;
-	font-family: var(--font-bold);
-}
-section {
 	display: flex;
-	width: 85em;
-	height: 700px;
-	margin: 0 0 10px 0;
+	flex-direction: column;
 	align-items: center;
-	justify-content: space-around;
-	& div {
-		flex: 2;
-	}
-	& p {
-		font-size: 20px;
-		line-height: 30px;
-	}
-	& h2 {
-		font-size: 36px;
-	}
-	& h4 {
-		font-size: 20px;
-	}
 }
-.image {
-	margin: 0 50px;
-	height: 75%;
-	flex: 3;
-	background-color: var(--color-unknown-image);
-	border-radius: 7px;
-	box-shadow: 0 2px 15px #181818;
-}
-.phoneImg {
-	flex: 1;
-}
-#overview .backgroundPic {
-	position: absolute;
-	right: 5%;
-	top: 15%;
-	transform: rotate(20deg);
-}
-#overview h1 {
-	font-size: 60px;
-}
+
 .section2 {
 	height: 100px;
-	margin-top: 50px;
-	margin-bottom: 40px;
+	margin: 30px 0;
 	& .downLink {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 	}
-	& .downLink:hover {
-		color: initial;
-	}
-	& .backgroundPic {
-		position: absolute;
-		left: 5%;
-		top: 80%;
-		transform: rotate(-40deg);
-	}
-}
-#features .backgroundPic {
-	position: absolute;
-	right: 5%;
-	top: 150%;
-	transform: rotate(-10deg);
-}
-.section4 .backgroundPic {
-	position: absolute;
-	left: 30%;
-	top: 190%;
-	transform: rotate(10deg);
-}
-.section5 .backgroundPic {
-	position: absolute;
-	left: 20%;
-	top: 340%;
-	transform: rotate(-100deg);
 }
 .section6 {
+	height: 300px;
+	padding: 100px 0;
+	align-items: flex-start;
+	text-align: justify;
 	& div {
 		margin: 0 20px;
 		line-height: 30px;
 	}
 	& p {
-		font-size: 16px;
+		font-size: var(--size-small-text);
 		color: var(--color-secondary);
 	}
 }
@@ -248,46 +176,23 @@ section {
 	align-items: center;
 	flex: none;
 }
-.heading {
+.sectionHead {
 	text-align: center;
 	width: 60%;
 	margin: 0;
 }
 .section7 div {
-	margin: 0 50px;
+	margin: 0 30px;
 }
 .section7 div p {
 	text-align: center;
 }
-.section7 .backgroundPic {
-	position: absolute;
-	left: 50%;
-	top: 450%;
-}
 .section7Images {
 	display: flex;
-	& .section7Img {
-		width: 270px;
-		height: 400px;
-		background-color: var(--color-unknown-image);
-		border-radius: 7px;
-		box-shadow: 0 2px 15px #181818;
-	}
 }
 .section8 {
-	margin-top: 100px;
-	padding-bottom: 100px;
+	margin-top: 70px;
+	padding-bottom: 80px;
 	height: fit-content;
-	& .backgroundPic {
-		position: absolute;
-		left: 30%;
-		bottom: -480%;
-		transform: rotate(160deg);
-	}
-}
-.backgroundPic {
-	filter: opacity(0.3);
-	width: 120px;
-	z-index: -1;
 }
 </style>
