e462b1431d655968c349aa185e628e0e80b32e65
Winechicken <dimonvinokurov@gmail.com>
2024-01-31 22:32:32 +0400

    created HomePage layout
    plus made small corrections

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,32 @@
-<script setup></script>
+<script setup>
+import WoodMechanics from '../components/common/WoodMechanics.vue'
+import MainPageHeader from '../components/homepage/header/MainPageHeader.vue'
+import Section from '../components/homepage/section/Section.vue'
+import CreateForm from '../components/homepage/section/CreateForm.vue'
+import RecentForm from '../components/homepage/section/RecentForm.vue'
+</script>
 
 <template>
+	<WoodMechanics />
+	<MainPageHeader />
 	<main>
-		ЭТО ГЛАВНАЯ СТРАНИЦА
+		<Section class="first"><CreateForm/></Section>
+		<Section><RecentForm /></Section>
 	</main>
 </template>
 
 <style scoped>
 main {
+	height: 100%;
 	font-family: var(--font-bold);
-	color: var(--color-text-hover);
+	color: var(--color-text);
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: center;
+	text-align: center;
+}
+.first{
+	background-color: var(--color-unknown-image);
 }
 </style>
