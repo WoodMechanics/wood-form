b30a07cc78790cd5a8205d2cb733d5fca6eab114
Daniil Savitskii <danildaniel121@gmail.com>
2024-02-24 23:26:52 +0400

    added better work with token

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,11 +1,26 @@
 import { Hono } from 'hono';
 import { form } from './forms.js';
+import { HTTPException } from 'hono/http-exception';
 
 export const api = new Hono();
 
 api.use('*', async (c, next) => {
-	const session = c.req.header('Authorization');
-	const token = session.split('Bearer')[1].trim();
+	const authHeader = c.req.header('Authorization');
+
+	const getToken = (authHeader) => {
+		if (authHeader.startsWith('Bearer ')) {
+			return authHeader.substring(7, authHeader.length);
+		} else {
+			return null;
+		}
+	};
+
+	const token = getToken(authHeader);
+
+	if (!token) {
+		throw new HTTPException(400, { message: 'Authorization error: Session is required' });
+	}
+
 	c.session = token;
 
 	await next();
