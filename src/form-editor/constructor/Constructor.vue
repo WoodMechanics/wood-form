2fb15c36eb174172de6617635065becca9a7895e
Daniil Savitskii <danildaniel121@gmail.com>
2024-02-26 21:29:35 +0400

    removed accessibility plugin

--- src/form-editor/constructor/Constructor.vue
+++ src/form-editor/constructor/Constructor.vue
@@ -1,5 +1,22 @@
-<script setup></script>
+<script setup>
+import SectionBlock from './SectionBlock.vue'
+</script>
 
 <template>
-	<div>Constructor</div>
+	<div class="constructor-wrapper">
+		<SectionBlock>
+			<label>
+				<input placeholder="Form name" type="text" />
+			</label>
+			<label>
+				<input placeholder="description" type="text" />
+			</label>
+		</SectionBlock>
+	</div>
 </template>
+
+<style scoped>
+.constructor-wrapper {
+	margin-top: 12px;
+}
+</style>
---
+++ src/form-editor/constructor/SectionBlock.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+	<div class="section-block">
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+.section-block {
+	width: 100%;
+	border: 1px solid black;
+}
+</style>
