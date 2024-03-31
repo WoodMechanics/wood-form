e3440782d0bc122829081e7394a086ef19d9cbc4
Daniil Savitskii <danildaniel121@gmail.com>
2024-03-31 15:24:01 +0400

    added cors to server

--- server/src/index.js
+++ server/src/index.js
@@ -2,16 +2,27 @@ import { Hono } from 'hono';
 import { api } from './routes/api.js';
 import { auth } from './routes/auth.js';
 import { setConfig } from './service/config.js';
-import { log } from './Demo.res.mjs';
-
-log();
+import { cors } from 'hono/cors';
 
 const app = new Hono();
 
 app.use('*', async (c, next) => {
 	setConfig(c.env);
+	auth.use('*');
 	await next();
 });
+
+app.use(
+	'*',
+	cors({
+		origin: 'http://localhost:5175',
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+		allowMethods: ['POST', 'GET', 'OPTIONS', 'DELETE'],
+		// exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
 
 app.route('/api', api);
 app.route('/auth', auth);
--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,33 +1,33 @@
 import { Hono } from 'hono';
 import { form } from './forms.js';
-import { HTTPException } from 'hono/http-exception';
+// import { HTTPException } from 'hono/http-exception';
 
 export const api = new Hono();
 
-api.use('*', async (c, next) => {
-	const authHeader = c.req.header('Authorization');
-
-	if (!authHeader) {
-		throw new HTTPException(401, { message: 'Unauthorized' });
-	}
-
-	const getToken = (authHeader) => {
-		if (authHeader.startsWith('Bearer ')) {
-			return authHeader.substring(7, authHeader.length);
-		} else {
-			return null;
-		}
-	};
-
-	const token = getToken(authHeader);
-
-	if (!token) {
-		throw new HTTPException(400, { message: 'Authorization error: Session is required' });
-	}
-
-	c.session = token;
-
-	await next();
-});
+// api.use('*', async (c, next) => {
+// 	const authHeader = c.req.header('Authorization');
+//
+// 	if (!authHeader) {
+// 		throw new HTTPException(401, { message: 'Unauthorized' });
+// 	}
+//
+// 	const getToken = (authHeader) => {
+// 		if (authHeader.startsWith('Bearer ')) {
+// 			return authHeader.substring(7, authHeader.length);
+// 		} else {
+// 			return null;
+// 		}
+// 	};
+//
+// 	const token = getToken(authHeader);
+//
+// 	if (!token) {
+// 		throw new HTTPException(400, { message: 'Authorization error: Session is required' });
+// 	}
+//
+// 	c.session = token;
+//
+// 	await next();
+// });
 
 api.route('/forms', form);
